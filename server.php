93b89290934c0a71cab34ef06ae60fda28424529
Ivori01 <93363134+Ivori01@users.noreply.github.com>
2023-10-29 22:45:02 -0600

    Update2

--- server.php
+++ server.php
@@ -13,7 +13,7 @@ $uri = urldecode(
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software1 here2023.
+// application without having installed a "real" web server software1 here20230505.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
